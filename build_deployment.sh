08b7914d47521a94092da46a5201f4dca2cbb43a
NickGalindo <ngalindoc@unal.edu.co>
2022-05-30 03:32:29 -0400

    finished mongo serialization and fixed reqs to not include current config

--- build_deployment.sh
+++ build_deployment.sh
@@ -6,4 +6,4 @@ rm -rf deployment-package.zip
 cd env/lib/python*/site-packages
 zip -r ${OLDPWD}/deployment-package.zip .
 cd $OLDPWD
-zip -g -r deployment-package.zip . -x "env*" "*__pycache__*" "*.zip" "test*" "build_deployment.sh" "requirements.txt" "*.git*"
+zip -g -r deployment-package.zip . -x "config.yaml" "env*" "*__pycache__*" "*.zip" "test*" "build_deployment.sh" "requirements.txt" "*.git*"
